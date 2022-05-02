b41c5de3f1b7718ee4db7ad34e453e8a017d65b6
Violetta <viol.fedoseenko@yandex.ru>
2022-05-02 18:33:29 +0300

    complete task

--- src/queue.js
+++ src/queue.js
@@ -12,23 +12,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.enqueue(3); // adds the element to the queue
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
+ * */
+//
+ 
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+ 
 class Queue {
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.nextEl = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    // если же у нас элементы есть, то мы должны получить ссылку на наш текущий 
+    // элемент(начало нашего списка)
+    if (this.head) {
+      let nextEl = this.head;
+      // пока у текущего элемента есть кто-то за ним,меняем ссылку на текщий элемент двигаясь к концу
+      while(nextEl.next) {
+        nextEl = nextEl.next;
+      }
+      this.nextEl.next = node;
+    }
+
+    //если нет головы(значит нет списка) мы создаем его. Теперь head равняется этому новому узлу и помещает элемент в конец очереди
+    if (!this.head) this.head = node;
+    this.nextEl = node;
+
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const currentHead = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+    return currentHead;
   }
+
 }
 
 module.exports = {
